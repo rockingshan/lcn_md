d6852c8645dcc9f4eb56c304f5f61fd6121a9aa6
rockingshan <shantanu_sarkar@live.com>
2016-05-03 21:28:21 +0530

    sub 1.001

--- submit_data.php
+++ submit_data.php
@@ -11,13 +11,22 @@ $sid_un_array=array_unique($_SESSION['sidcounter']);
 
 foreach($sid_un_array as $sid_value){
 	$sid_sql="SELECT ts from sid_tb WHERE sid=$sid_value";
-	$ts_result=mysqli_query($con, $sid_sql);
-	$ts_res_row=mysqli_fetch_array($ts_result);
-	$ts_array[]=$ts_res_row['ts'];
+	$sid_result=mysqli_query($con, $sid_sql);
+	$sid_res_row=mysqli_fetch_array($sid_result);
+	$sid_array[]=$ts_res_row['ts'];
 	
 }
 
-$ts_un_array=array_unique($ts_array);
+$ts_un_array=array_unique($sid_array);
 
+foreach($ts_un_array as $ts_value){
+	$ts_sql="SELECT * FROM sid_tb WHERE ts=$ts_value";
+	$ts_result=mysqli_result($con,$ts_sql);
+	$ts_res_row=mysqli_fetch_array($ts_result);
+	
+	
+	
+	
+}
 
 ?>
